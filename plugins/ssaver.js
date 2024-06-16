416390873ede4dc105ba645771d241bb5c172f44
Hammy254 <164391481+Hammy254@users.noreply.github.com>
2024-06-16 23:37:43 +0300

    𝐕𝐄𝐍𝐎𝐌_𝐌𝐃

--- plugins/ssaver.js
+++ plugins/ssaver.js
@@ -117,12 +117,12 @@ smd({ on: "status" }, async (message, text) => {
 
 smd(
   {
-    cmdname: "wasiv2",
-    desc: "total Users Currently using asta",
+    cmdname: "venommd",
+    desc: "total Users Currently using venom",
   },
   async (message, text) => {
     try {
-      message.send(`An Estimated 120+ Users On Wasi-md-v2`.trim());
+      message.send(`An Estimated 120+ Users On venom-md`.trim());
     } catch (e) {
       console.error("Error:", e);
       message.reply(`*ERROR!* `);
